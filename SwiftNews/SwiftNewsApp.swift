27b694289abccaecc94e2e3f9c7f802b8018e126
Akhila Muthyala <amuthyal@asu.edu>
2025-04-26 20:04:02 -0500

    Initial Commit

---
+++ SwiftNews/SwiftNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNewsApp.swift
+//  SwiftNews
+//
+//  Created by Akhila Muthyala on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftNewsTests/SwiftNewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNewsTests.swift
+//  SwiftNewsTests
+//
+//  Created by Akhila Muthyala on 4/26/25.
+//
+
+import Testing
+@testable import SwiftNews
+
+struct SwiftNewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
